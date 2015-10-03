ffc6bc610551f1d1f9d0aab3cb7e7d7c82b93445
Jakub Plichta <jakub.plichta@gmail.com>
2015-10-03 15:30:03 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class Tox(TestCommand):
 
 params = {
     'name': 'grafana-dashboard-builder',
-    'version': '0.1.0a7',
+    'version': '0.1.0a8',
     'packages': [
         'grafana_dashboards',
         'grafana_dashboards.client',
