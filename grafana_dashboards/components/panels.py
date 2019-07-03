f016e91c521dd1de8a50d86cb1a04dc6d084a720
trampfox <trampfox@gmail.com>
2019-07-03 09:40:25 +0200

    Another flake fix.

--- grafana_dashboards/components/panels.py
+++ grafana_dashboards/components/panels.py
@@ -35,7 +35,7 @@ class PanelsItemBase(JsonGenerator):
 class Graph(PanelsItemBase):
 
     _copy_fields = {'stack', 'fill', 'aliasColors', 'leftYAxisLabel', 'bars', 'lines', 'linewidth', 'y_formats',
-                    'x-axis', 'y-axis', 'points', 'pointradius', 'percentage', 'steppedLine', 'repeat', 
+                    'x-axis', 'y-axis', 'points', 'pointradius', 'percentage', 'steppedLine', 'repeat',
                     'repeatDirection', 'decimals', 'minSpan', 'datasource', 'description'}
 
     def gen_json_from_data(self, data, context):
